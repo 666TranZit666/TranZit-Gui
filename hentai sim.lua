5f91a82c8fa7af887ffe3266780962a55f99801f
666TranZit666 <92951291+666TranZit666@users.noreply.github.com>
2022-08-17 20:55:42 -0700

    Update hentai sim.lua

--- hentai sim.lua
+++ hentai sim.lua
@@ -1,4 +1,10 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/666TranZit666/Misc-GUI/main/Skull"),true))()
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
 while true do
     wait(1)
 local resetEggs = function()
