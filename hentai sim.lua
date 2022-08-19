cb5a0dc06f7df9f4fbaa4666fd8707275d88d168
666TranZit666 <92951291+666TranZit666@users.noreply.github.com>
2022-08-18 20:19:13 -0700

    Update hentai sim.lua

--- hentai sim.lua
+++ hentai sim.lua
@@ -6,7 +6,7 @@ game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
 while true do
-    wait(1)
+    wait(5)
 local resetEggs = function()
   local rootPart = game.Players.LocalPlayer.Character.PrimaryPart
   firetouchinterest(rootPart,game.Workspace.Stage6.RebirthButton,0)
